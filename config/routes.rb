b7fde40c61d2f85b8a743e571ff2565693e512a6
Prafulla <prafulla.vispute@gmail.com>
2017-10-02 22:27:19 +0800

    Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  get 'New' => 'projects#new'
+
+   get 'Home' => 'projects#index'
+
   root to: 'projects#index'
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
